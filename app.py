bd8a2b5fccd0ebe98df446ae1789305a76f4f0fe
i-am-zach <zachlefko@gmail.com>
2021-03-11 19:15:57 -0600

    Intial heroku commit

--- app.py
+++ app.py
@@ -12,30 +12,30 @@ app.layout = html.Div(className="content container", children=[
     html.H1(children="Group 1: Zach Lefkovitz, Aidan Morrison, Nathan Oleari, Nick Larson"),
     html.Div(className="columns", children=[
         html.Div(className="column", children=[
-            dcc.Graph(id="group-1-surf", figure=surface_plot(*generate_numpy("group_1.csv"))),
+            dcc.Graph(id="group-1-surf", figure=surface_plot(*generate_numpy("https://raw.githubusercontent.com/i-am-zach/Physics-Voltage-Lab/main/group_1.csv"))),
         ]),
         html.Div(className="column", children=[
-            dcc.Graph(id="group-1-scat", figure=scatter_plot(generate_formatted_dataframe("group_1.csv"))),
+            dcc.Graph(id="group-1-scat", figure=scatter_plot(generate_formatted_dataframe("https://raw.githubusercontent.com/i-am-zach/Physics-Voltage-Lab/main/group_1.csv"))),
         ])
     ]),
 
     html.H1(children="Group 2: Meghan Stancliff, Adam Jankelowitz, Nick Larson, Michelle Chen, Zach Lefkovitz"),
     html.Div(className="columns", children=[
         html.Div(className="column", children=[
-            dcc.Graph(id="group-2-surf", figure=surface_plot(*generate_numpy("group_2.csv"))),
+            dcc.Graph(id="group-2-surf", figure=surface_plot(*generate_numpy("https://raw.githubusercontent.com/i-am-zach/Physics-Voltage-Lab/main/group_2.csv"))),
         ]),
         html.Div(className="column", children=[
-            dcc.Graph(id="group-2-scat", figure=scatter_plot(generate_formatted_dataframe("group_2.csv"))),
+            dcc.Graph(id="group-2-scat", figure=scatter_plot(generate_formatted_dataframe("https://raw.githubusercontent.com/i-am-zach/Physics-Voltage-Lab/main/group_2.csv"))),
         ])
     ]),
 
     html.H1(children="Group 3: Ari Wainer, Javi Espanosa, Nathan Oleari, Daniel Malis"),
     html.Div(className="columns", children=[
         html.Div(className="column", children=[
-            dcc.Graph(id="group-3-surf", figure=surface_plot(*generate_numpy("group_3.csv"))),
+            dcc.Graph(id="group-3-surf", figure=surface_plot(*generate_numpy("https://raw.githubusercontent.com/i-am-zach/Physics-Voltage-Lab/main/group_3.csv"))),
         ]),
         html.Div(className="column", children=[
-            dcc.Graph(id="group-3-scat", figure=scatter_plot(generate_formatted_dataframe("group_3.csv"))),
+            dcc.Graph(id="group-3-scat", figure=scatter_plot(generate_formatted_dataframe("https://raw.githubusercontent.com/i-am-zach/Physics-Voltage-Lab/main/group_3.csv"))),
         ])
     ]),
 ])
